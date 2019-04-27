c1232c9d85c610b0767d7002ddeaca574479df55
296738841 <296738841@qq.com>
2019-04-27 18:11:22 +0800

    hello git

--- src/main/java/com/dong/appmain/BIOMain.java
+++ src/main/java/com/dong/appmain/BIOMain.java
@@ -3,5 +3,6 @@ package com.dong.appmain;
 public class BIOMain {
     public static void main(String[] args) {
         System.out.println("hello");
+        System.out.println("hello git , i am coming");
     }
 }
